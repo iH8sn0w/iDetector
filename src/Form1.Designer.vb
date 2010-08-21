5784c642063dff2e4120893dbe2552ca0bddb7d7
iH8sn0w <ih8sn0wydaz@gmail.com>
2010-08-21 17:39:45 -0400

    First Commit

---
+++ src/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.console = New System.Windows.Forms.RichTextBox
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(39, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(436, 30)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Pop your iPhone 3G[S] into DFU mode and press the button below! " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Ensure iTunes i" & _
+            "s installed!"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(13, 51)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(474, 107)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Is my Bootrom Old or New?"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Blue
+        Me.Label2.Location = New System.Drawing.Point(388, 161)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(109, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "DFU Mode Help"
+        '
+        'console
+        '
+        Me.console.Location = New System.Drawing.Point(13, 196)
+        Me.console.Name = "console"
+        Me.console.Size = New System.Drawing.Size(1, 1)
+        Me.console.TabIndex = 3
+        Me.console.Text = ""
+        Me.console.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(499, 179)
+        Me.Controls.Add(Me.console)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "iDetector -- By: iH8sn0w"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents console As System.Windows.Forms.RichTextBox
+
+End Class
---
+++ src/Form1.vb
@@ -0,0 +1,136 @@
+﻿Imports System
+Imports System.Management
+Imports System.Windows.Forms
+Public Class Form1
+    Public iBoot As String = ""
+    Public CPID As String = ""
+    Sub Delay(ByVal dblSecs As Double)
+
+        Const OneSec As Double = 1.0# / (1440.0# * 60.0#)
+        Dim dblWaitTil As Date
+        Now.AddSeconds(OneSec)
+        dblWaitTil = Now.AddSeconds(OneSec).AddSeconds(dblSecs)
+        Do Until Now > dblWaitTil
+            Application.DoEvents() ' Allow windows messages to be processed
+        Loop
+
+    End Sub
+    Function HighlightWords(ByVal rtb As RichTextBox, ByVal sFindString As String, ByVal lColor As System.Drawing.Color) As Integer
+
+        Dim iFoundPos As Integer 'Position of first character of match
+        Dim iFindLength As Integer       'Length of string to find
+        Dim iOriginalSelStart As Integer
+        Dim iOriginalSelLength As Integer
+        Dim iMatchCount As Integer      'Number of matches
+
+        'Save the insertion points current location and length
+        iOriginalSelStart = rtb.SelectionStart
+        iOriginalSelLength = rtb.SelectionLength
+
+        'Cache the length of the string to find
+        iFindLength = Len(sFindString) + 16
+
+        'Attempt to find the first match
+        iFoundPos = rtb.Find(sFindString, 0, RichTextBoxFinds.NoHighlight)
+        While iFoundPos > 0
+            iMatchCount = iMatchCount + 1
+
+            console.SelectionStart = iFoundPos
+            'The SelLength property is set to 0 as soon as you change SelStart
+            console.SelectionLength = iFindLength
+            'rtb.SelectionBackColor = lColor
+
+            console.Select(iFoundPos, iFindLength - 8)
+            iBoot = console.SelectedText
+            'MsgBox(iBoot)
+            'Attempt to find the next match
+            iFoundPos = rtb.Find(sFindString, iFoundPos + iFindLength, RichTextBoxFinds.NoHighlight)
+        End While
+
+        'Restore the insertion point to its original location and length
+        rtb.SelectionStart = iOriginalSelStart
+        rtb.SelectionLength = iOriginalSelLength
+
+        'Return the number of matches
+        HighlightWords = iMatchCount
+    End Function
+    Function HighlightWords2(ByVal rtb As RichTextBox, ByVal sFindString2 As String, ByVal lColor2 As System.Drawing.Color) As Integer
+
+        Dim iFoundPos2 As Integer 'Position of first character of match
+        Dim iFindLength2 As Integer       'Length of string to find
+        Dim iOriginalSelStart2 As Integer
+        Dim iOriginalSelLength2 As Integer
+        Dim iMatchCount2 As Integer      'Number of matches
+
+        'Save the insertion points current location and length
+        iOriginalSelStart2 = rtb.SelectionStart
+        iOriginalSelLength2 = rtb.SelectionLength
+
+        'Cache the length of the string to find
+        iFindLength2 = Len(sFindString2)
+
+        'Attempt to find the first match
+        iFoundPos2 = rtb.Find(sFindString2, 0, RichTextBoxFinds.NoHighlight)
+        While iFoundPos2 > 0
+            iMatchCount2 = iMatchCount2 + 1
+
+            console.SelectionStart = iFoundPos2
+            'The SelLength property is set to 0 as soon as you change SelStart
+            console.SelectionLength = iFindLength2
+            'rtb.SelectionBackColor = lColor2
+
+            console.Select(iFoundPos2 + 5, iFindLength2 - 1)
+            CPID = console.SelectedText
+            'MsgBox(CPID)
+            'Attempt to find the next match
+            iFoundPos2 = rtb.Find(sFindString2, iFoundPos2 + iFindLength2, RichTextBoxFinds.NoHighlight)
+        End While
+
+        'Restore the insertion point to its original location and length
+        rtb.SelectionStart = iOriginalSelStart2
+        rtb.SelectionLength = iOriginalSelLength2
+
+        'Return the number of matches
+        HighlightWords2 = iMatchCount2
+    End Function
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Button1.Enabled = False
+        Button1.Text = "Searching for DFU..."
+        Dim searcher As New ManagementObjectSearcher( _
+                    "root\CIMV2", _
+                    "SELECT * FROM Win32_PnPDevice")
+
+        For Each queryObj As ManagementObject In searcher.Get()
+            console.Text += ("SystemElement: {0}" & queryObj("SystemElement"))
+        Next
+        If console.Text.Contains("ECID") Then
+            Dim icountMatch2 As Integer
+            icountMatch2 = HighlightWords2(console, "CPID:", System.Drawing.Color.Red)
+            If CPID = "8920" Then
+                Call GoGoGadgetiBoot()
+            Else
+                MsgBox("Another device other than a 3GS was detected! Please connect ONLY a 3GS.", MsgBoxStyle.Critical)
+            End If
+        Else
+            MsgBox("No Device was Detected!")
+            Button1.Enabled = True
+            Button1.Text = "Is my Bootrom Old or New?"
+        End If
+    End Sub
+    Public Sub GoGoGadgetiBoot()
+        Dim icountMatch As Integer
+        icountMatch = HighlightWords(console, "IBOOT", System.Drawing.Color.Red)
+        If iBoot = "IBOOT-359.3]""" Then
+            MsgBox("Your iPhone 3G[S] contains the Old Bootrom." & Chr(13) & Chr(13) & "You can Exit DFU Mode by holding the Power + Home button continuously.")
+            Button1.Enabled = True
+            Button1.Text = "Is my Bootrom Old or New?"
+        Else
+            MsgBox("Your iPhone 3G[S] contains the NEW Bootrom." & Chr(13) & Chr(13) & "You can Exit DFU Mode by holding the Power + Home button continuously.")
+            Button1.Enabled = True
+            Button1.Text = "Is my Bootrom Old or New?"
+        End If
+    End Sub
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+        Process.Start("http://www.youtube.com/watch?v=bITIiGswjFI")
+    End Sub
+End Class
